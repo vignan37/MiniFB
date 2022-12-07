44c1c759554c05ba9af7700ea1ccbecb389a736b
nv4as-win <nv4as-win@nv4as-win.cu1faf33r1nezjwzqlz4mbbgje.phxx.internal.cloudapp.net>
2022-12-07 12:02:29 +0000

    messages class

--- MiniFB/src/main/java/com/realTimeChatApp/MiniFB/Model/Message.java
+++ MiniFB/src/main/java/com/realTimeChatApp/MiniFB/Model/Message.java
@@ -1,5 +1,42 @@
 package com.realTimeChatApp.MiniFB.Model;
 
 public class Message {
+	private String sender;
+	private String data;
+	private String timeStamp;
+	
+	public Message() {
+		
+	}
+	
+	public Message(String sender, String data) {
+		this.sender = sender;
+		this.data = data;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
 
 }
