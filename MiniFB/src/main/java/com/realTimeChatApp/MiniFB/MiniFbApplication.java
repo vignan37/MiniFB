da370fc9b89cc8fb74b81afa7a6fe3d149a54d5d
nv4as-win <nv4as-win@nv4as-win.cu1faf33r1nezjwzqlz4mbbgje.phxx.internal.cloudapp.net>
2022-12-07 07:11:57 +0000

    Initial commit

---
+++ MiniFB/src/main/java/com/realTimeChatApp/MiniFB/MiniFbApplication.java
@@ -0,0 +1,13 @@
+package com.realTimeChatApp.MiniFB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniFbApplication.class, args);
+	}
+
+}
---
+++ MiniFB/src/test/java/com/realTimeChatApp/MiniFB/MiniFbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realTimeChatApp.MiniFB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
